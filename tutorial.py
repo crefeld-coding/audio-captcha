491b5ac27412949ac11bb064ecd0ac2091ae77b9
leo-at-crefeld <47148174+leo-at-crefeld@users.noreply.github.com>
2019-04-09 08:48:43 -0400

    commented out stuff about audio files

--- tutorial.py
+++ tutorial.py
@@ -4,13 +4,14 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-harvard = sr.AudioFile('harvard.wav')
-jackhammer = sr.AudioFile('jackhammer.wav')
-with harvard as source:
-    audio1 = r.record(source, duration=4)
-with jackhammer as source:
-    r.adjust_for_ambient_noise(source, duration=.5)
-    audio2 = r.record(source, duration=4)
+# Speech Recognition Stuff:
+# harvard = sr.AudioFile('harvard.wav')
+# jackhammer = sr.AudioFile('jackhammer.wav')
+# with harvard as source:
+#     audio1 = r.record(source, duration=4)
+# with jackhammer as source:
+#     r.adjust_for_ambient_noise(source, duration=.5)
+#     audio2 = r.record(source, duration=4)
 
 print(r.recognize_google(audio1) + '  !=  ' + r.recognize_google(audio2))
 
