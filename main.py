fcf83f98fc85730d389a67862e744043947ddb4c
leo-at-crefeld <47148174+leo-at-crefeld@users.noreply.github.com>
2019-04-09 09:26:43 -0400

    wrote first portion of main
    using audio, prompts user, records responce and then prints responce

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 """Audio-Captcha Main"""
+
+import speech_recognition as sr
+import pyttsx3 as tts
+
+# Global Vars
+r = sr.Recognizer()
+mic = sr.Microphone()
+engine = tts.init()
+
+
+def voice_input(prompt):
+
+    engine.say("Please repeat the following pass phrase")
+    engine.say(prompt)
+    engine.runAndWait()
+
+    with mic as source:
+        try:
+            audio = r.listen(source)
+            engine.say("Please wait")
+            engine.runAndWait()
+            return r.recognize_google(audio)
+
+        except sr.UnknownValueError:
+            pass
+
+
+print(voice_input("swordfish"))
