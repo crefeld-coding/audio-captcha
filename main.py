d4062f7f5bbe60e9903333684014ac15cf94f8dd
leo-at-crefeld <47148174+leo-at-crefeld@users.noreply.github.com>
2019-04-10 09:26:44 -0400

    wrote some version 0 stuff and some debugging

--- main.py
+++ main.py
@@ -2,28 +2,52 @@
 
 import speech_recognition as sr
 import pyttsx3 as tts
+import random
 
 # Global Vars
 r = sr.Recognizer()
 mic = sr.Microphone()
 engine = tts.init()
+PASSPHRASES = ("swordfish", "alpha", "bravo", "charlie", "delta", "echo", "foxtrot")
 
 
-def voice_input(prompt):
+def voice_input(passphrase):
 
     engine.say("Please repeat the following pass phrase")
-    engine.say(prompt)
+    engine.say(passphrase)
     engine.runAndWait()
 
     with mic as source:
         try:
             audio = r.listen(source)
+            transcription = r.recognize_google(audio)
+            print(f"Input was: {transcription}")
             engine.say("Please wait")
             engine.runAndWait()
-            return r.recognize_google(audio)
+            return transcription
 
         except sr.UnknownValueError:
-            pass
+            print("unrecognizable input")
+            return "unrecognizable input"
 
 
-print(voice_input("swordfish"))
+def construct_passphrase():
+    """Creates a passphrase of 3 words from the global tuple PASSPHRASES, returns a string"""
+    global PASSPHRASES
+    passphrase = str()
+    for i in range(3):
+        passphrase += (PASSPHRASES[random.randint(0, len(PASSPHRASES))] + " ")
+    return passphrase
+
+
+def verify_human():
+    passphrase = construct_passphrase().lower()
+    passphrase_entered = voice_input(passphrase).lower()
+    if passphrase_entered == passphrase:
+        print("Test Cleared")
+    else:
+        print("Test Failed")
+
+
+if __name__ == "__main__":
+    verify_human()
