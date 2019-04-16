12a91a6c4d2f508a171e64ed42e63112f58c3097
leo-at-crefeld <47148174+leo-at-crefeld@users.noreply.github.com>
2019-04-16 08:39:45 -0400

    removed code for tutorial files

--- tutorial.py
+++ tutorial.py
@@ -5,15 +5,6 @@ import speech_recognition as sr
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-# Speech Recognition Stuff:
-# harvard = sr.AudioFile('harvard.wav')
-# jackhammer = sr.AudioFile('jackhammer.wav')
-# with harvard as source:
-#     audio1 = r.record(source, duration=4)
-# with jackhammer as source:
-#     r.adjust_for_ambient_noise(source, duration=.5)
-#     audio2 = r.record(source, duration=4)
-
 # Microphone stuff
 with mic as source:
 
